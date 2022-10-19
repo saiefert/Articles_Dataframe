43eefd3cba5754e3ec51b33a7b10995d52dc9da2
lucasSintraWarren <lucas.sintra@warren.com.br>
2022-10-18 22:37:28 -0300

    fix coluna type_publication

--- python/main.py
+++ python/main.py
@@ -46,11 +46,33 @@ class ArticlesReader():
             com as colunas solicitadas.
         """
         for column in df.columns:
+            if column == "ENTRYTYPE":
+                df.rename(
+                    columns={'ENTRYTYPE': 'type_publication'},
+                    inplace=True
+                    )
+                column = "type_publication"
+                print('Coluna renomeada')
             if column not in columns_list:
                 df = df.drop(column, axis="columns")
                 print(f"Dataframe com coluna retirada: {column}")
 
         return df
+
+    def concat_dfs(df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
+        """Concatena os dataframes.
+
+        Args:
+            df_1 (pd.DataFrame): que será concatenado
+            df_2 (pd.DataFrame): que será concatenado
+
+        Returns:
+            pd.DataFrame: dataframe já concatenado
+        """
+        df_concat = pd.DataFrame()
+        df_concat = pd.concat([df_1, df_2])
+
+        return df_concat
 
     def save_df(df: pd.DataFrame, file_type: str) -> None:
         """Salva o DataFrame no formato solicitado.
@@ -116,9 +138,15 @@ if __name__ == '__main__':
 
     task = ArticlesReader
 
-    df = task.read_files(path_files='../dados')
+    df_IEEE = task.read_files(path_files='../dados/IEEE')
+    df_ACM = task.read_files(path_files='../dados/ACM')
+    df_SDC = task.read_files(path_files='../dados/SDC')
+
+    df_main = task.concat_dfs(df_1=df_SDC, df_2=df_IEEE)
+    df_main = task.concat_dfs(df_1=df_main, df_2=df_ACM)
+
     df_clean = task.cleaner_columns(
-        df=df,
+        df=df_main,
         columns_list=config['chosen_columns']
         )
     task.save_df(df=df_clean, file_type=config['file_saved_format'])
